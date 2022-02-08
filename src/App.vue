832dc6fd7cf0dff1f2fcc970d3d4051b5ddab80c
SimoMassaro52 <simonesimomassaro@gmail.com>
2022-02-08 19:15:45 +0100

    Timer structure ready

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-   
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-  
+    Header
   }
 }
 </script>
 
 <style lang="scss">
 @import './assets/style/global.scss';
+/*BOOTSTRAP ICONS*/
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css");
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <header>
+
+      <!-- TIMER SUB SECTION -->
+      <section id="timer" class="d-flex justify-content-center">
+          <div id="clock" class="d-flex align-items-center justify-content-between">
+              <span>Starts TOMORROW! Our biggest event of the year...</span>
+              <span class="mx-3"><i class="bi bi-clock"></i> 00 : 00 : 00 : 00</span>
+          </div>
+          <div class="d-flex align-items-center px-5">
+              <div class="custom_btn">
+                  <span>Get ticket</span>
+              </div>
+          </div>
+      </section>
+
+        <!-- NAV -->
+      <nav class="d-flex align-items-center">
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
+/*TIMER SUB SECTION*/
+#timer{
+    background-color: $silver;
+    height: 50px;
+}
+
+#clock{
+    span:first-child{
+        color: $boulder;
+    }
+}
+
+.custom_btn{
+    background-color: $button-orange;
+    color: white;
+    padding: 5px 30px;
+    border-radius: 5px;
+    font-weight: 700;
+    cursor: pointer;
+}
+
+/*NAV*/
+
+
+
+</style>
